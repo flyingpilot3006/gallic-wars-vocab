cabff3ffa052574f50e702a2fa119de6859dee53
Joseph Jacobson <flyingpilot3006@users.noreply.github.com>
2026-05-05 08:52:35 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // CRITICAL: This matches your GitHub repository name
+  // This must match your GitHub repository name exactly, with leading and trailing slashes.
   base: '/gallic-wars-vocab/',
 })
